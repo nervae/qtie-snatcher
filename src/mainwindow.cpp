a784ff6847060ffc5e59fed83f7b04be8653c0fc
averne <averne381@gmail.com>
2021-03-13 20:51:51 +0100

    Display image

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,10 @@
+#include <QPixmap>
+
 #include "mainwindow.hpp"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(std::make_unique<Ui::MainWindow>()) {
-   ui->setupUi(this);
+   this->ui->setupUi(this);
+
+   this->ui->label->setPixmap(QPixmap(""));
+   this->ui->label->setScaledContents(true);
 }
